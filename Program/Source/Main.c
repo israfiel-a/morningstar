79f0139460d3abc94bdd1d136bbf2fbfc7d0004b
Israfiel <174603598+israfiel-a@users.noreply.github.com>
2024-08-09 03:42:16 -0400

    oops left test code in main

--- Program/Source/Main.c
+++ Program/Source/Main.c
@@ -3,10 +3,6 @@
 
 int main(int argc, char** argv)
 {
-    ptr_t ptr = AllocateBlock(125);
-    ShrinkBlockSafe(&ptr, 21);
-    FreeBlock(&ptr);
-
     BeginSession(argc, argv);
     CleanupSession();
 }
